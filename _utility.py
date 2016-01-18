fa75867b50873f7be652d6555efc84ce2983ac74
Brent Nowak <brent613@gmail.com>
2016-01-17 16:20:46 -0800

    update for 1.1 release

---
+++ _utility.py
@@ -0,0 +1,126 @@
+#############################
+# Dependencies
+#############################
+
+import psycopg2
+import ConfigParser
+import pandas as pd
+
+#############################
+# Database
+#############################
+
+config = ConfigParser.ConfigParser()
+config.read(["config.ini"])
+password = config.get("DATABASE", "password")
+user = config.get("DATABASE", "user")
+host = config.get("DATABASE", "host")
+port = config.get("DATABASE", "port")
+database_spotmarket = config.get("DATABASE", "database")
+
+# Connection String
+conn_string_spotmarket = "host=%s dbname=%s user=%s password=%s" % (host, database_spotmarket, user, password)
+
+
+# Console
+# Display wider outputs when debugging
+desired_width = 320
+pd.set_option('display.width', desired_width)
+
+
+#############################
+# Functions
+#############################
+
+
+#
+# Input     regionID
+# Output    regionName
+#
+def regionname(regionID):
+    conn = psycopg2.connect(conn_string_spotmarket)
+    cursor = conn.cursor()
+    sql = 'SELECT "mapRegions"."regionName" FROM public."mapRegions" WHERE "mapRegions"."regionID" = %s'
+    data = (regionID, )
+    cursor.execute(sql, data)
+    result = cursor.fetchall()
+    cursor.close()
+    return result
+
+
+#
+# Input     regionID
+# Output    dataframe of SUM_factionKills grouped by timestamp
+#
+def getnpckills_byregion(regionID):
+    conn = psycopg2.connect(conn_string_spotmarket)
+    cursor = conn.cursor()
+    sql = """SELECT
+      SUM(mapkills."factionKills") as SUM_factionKills, timestamp
+    FROM
+      data.mapkills,
+      public."mapSolarSystems"
+    WHERE
+      mapkills."solarSystemID" = "mapSolarSystems"."solarSystemID"
+        AND public."mapSolarSystems"."regionID" = %s
+    GROUP BY mapkills."timestamp"
+    ORDER BY timestamp DESC
+    """
+    data = (regionID, )
+    cursor.execute(sql, data)
+    df = pd.DataFrame(cursor.fetchall(),columns=['SUM_factionKills','timestamp'])
+    cursor.close()
+    return df
+
+#
+# Input         mapapi_data
+# Output        Database insert
+#
+def insertmap(mapapi_data):
+    count_insert = 0
+    for key,value in mapapi_data.iteritems():
+        id = value['id']
+        ship = value['ship']
+        faction = value['faction']
+        pod = value['pod']
+        mapinsertrecord(maptimestamp, id, ship, faction, pod)
+        count_insert += 1
+    return count_insert
+
+#
+# Insert mapapi_data record
+#
+def mapinsertrecord(timestamp, id, ship, faction, pod):
+    conn = psycopg2.connect(conn_string)
+    cursor = conn.cursor()
+    sql = 'INSERT INTO data."mapkills" (timestamp, "solarSystemID", "shipKills", "factionKills", "podKills") VALUES (%s, %s, %s, %s, %s)'
+    data = (timestamp, id, ship, faction, pod, )
+    cursor.execute(sql, data)
+    conn.commit()
+    return 0
+
+#
+# Input         jumpsapi_data
+# Output        Database insert
+#
+def insertjumps(jumps_data):
+    count_insert = 0
+    for key,value in jumpsapi_data.iteritems():
+        id = key
+        jumps = value
+        jumpsinsertrecord(jumpstimestamp, id, jumps)
+        count_insert += 1
+    return count_insert
+
+#
+# Insert jumpsapi_data record
+#
+def jumpsinsertrecord(timestamp, id, jumps):
+    conn = psycopg2.connect(conn_string)
+    cursor = conn.cursor()
+    sql = 'INSERT INTO data."mapjumps" (timestamp, "solarSystemID", "shipJumps") VALUES (%s, %s, %s)'
+    data = (timestamp, id, jumps, )
+    cursor.execute(sql, data)
+    conn.commit()
+    return 0
+
--- consumer.py
+++ consumer.py
@@ -4,24 +4,15 @@
 #-----------------------------------------------------------------------------
 # Version: 1.0
 # - Initial release
+# Version: 1.1
+# - Remove record check function before INSERT
+# - Move configuration and functions to _utility.py
 #-----------------------------------------------------------------------------
 
 import arrow
-import psycopg2
 import evelink.map
-import ConfigParser
 
-# Configuration
-config = ConfigParser.ConfigParser()
-config.read(["config.ini"])
-host = config.get("DATABASE", "host")
-port = config.get("DATABASE", "port")
-database = config.get("DATABASE", "database")
-user = config.get("DATABASE", "user")
-password = config.get("DATABASE", "password")
-
-# Database connection
-conn_string = "host=%s dbname=%s user=%s password=%s" % (host, database, user, password)
+from _utility import *
 
 # API
 mapapi = evelink.map.Map()
@@ -30,9 +21,7 @@ jumpsapi = evelink.map.Map()
 jumpresponse = jumpsapi.jumps_by_system()
 
 # Convert timestamps to Arrow
-mapapi_dataTime = arrow.get(mapresponse.timestamp)
 mapapi_cachedUntil = arrow.get(mapresponse.expires)
-jumps_dataTime = arrow.get(jumpresponse.timestamp)
 jumps_cachedUntil = arrow.get(jumpresponse.expires)
 
 # Get dictionary
@@ -44,79 +33,6 @@ maptimestamp = mapapi_cachedUntil.format('YYYY-MM-DD HH:mm:ss')
 jumpstimestamp = jumps_cachedUntil.format('YYYY-MM-DD HH:mm:ss')
 
 
-#
-# Map functions
-#
-
-
-def mapcheckrowexists(timestamp, id, ship, faction, pod):
-    conn = psycopg2.connect(conn_string)
-    cursor = conn.cursor()
-    sql = 'SELECT * FROM data."mapkills" WHERE timestamp = %s AND "mapkills"."solarSystemID" = %s AND "mapkills"."shipKills" = %s and "mapkills"."factionKills" = %s AND "mapkills"."podKills" = %s'
-    data = (timestamp, id, ship, faction, pod, )
-    cursor.execute(sql, data)
-    result = cursor.fetchall()
-    return result
-
-
-def mapinsertrecord(timestamp, id, ship, faction, pod):
-    conn = psycopg2.connect(conn_string)
-    cursor = conn.cursor()
-    sql = 'INSERT INTO data."mapkills" (timestamp, "solarSystemID", "shipKills", "factionKills", "podKills") VALUES (%s, %s, %s, %s, %s)'
-    data = (timestamp, id, ship, faction, pod, )
-    cursor.execute(sql, data)
-    conn.commit()
-    return 0
-
-def insertmap(mapapi_data):
-    count_insert = 0
-    for key,value in mapapi_data.iteritems():
-        id = value['id']
-        ship = value['ship']
-        faction = value['faction']
-        pod = value['pod']
-        if len(mapcheckrowexists(maptimestamp, id, ship, faction, pod)) == 0:
-            mapinsertrecord(maptimestamp, id, ship, faction, pod)
-            count_insert += 1
-    return count_insert
-
-
-#
-# Jumps functions
-#
-
-
-def jumpcheckrowexists(timestamp, id, jumps):
-    conn = psycopg2.connect(conn_string)
-    cursor = conn.cursor()
-    sql = 'SELECT * FROM data."mapjumps" WHERE timestamp = %s AND "mapjumps"."solarSystemID" = %s AND "mapjumps"."shipJumps" = %s'
-    data = (timestamp, id, jumps, )
-    cursor.execute(sql, data)
-    result = cursor.fetchall()
-    return result
-
-
-def jumpsinsertrecord(timestamp, id, jumps):
-    conn = psycopg2.connect(conn_string)
-    cursor = conn.cursor()
-    sql = 'INSERT INTO data."mapjumps" (timestamp, "solarSystemID", "shipJumps") VALUES (%s, %s, %s)'
-    data = (timestamp, id, jumps, )
-    cursor.execute(sql, data)
-    conn.commit()
-    return 0
-
-
-def insertjumps(jumps_data):
-    count_insert = 0
-    for key,value in jumpsapi_data.iteritems():
-        id = key
-        jumps = value
-        if len(jumpcheckrowexists(jumpstimestamp, id, jumps)) == 0:
-            jumpsinsertrecord(jumpstimestamp, id, jumps)
-            count_insert += 1
-    return count_insert
-
-
 def main():
     # Run Map import
     count_mapinsert = insertmap(mapapi_data)
@@ -125,6 +41,7 @@ def main():
     # Run Jumps import
     count_jumpsinsert = insertjumps(jumpsapi_data)
     print("[" + str(jumpstimestamp) + "][Informational] Jumps Inserted: " + str(count_jumpsinsert))
+
 
 if __name__ == "__main__":
     main()
