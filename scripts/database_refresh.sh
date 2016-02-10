3286dd3a5788d8364dc6785b0d15e76293ff6fdd
Brent Nowak <brent613@gmail.com>
2016-02-09 17:46:33 -0800

    initial sbadmin2 commit

---
+++ scripts/database_refresh.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /home/ubuntu/spotmarket
+wget https://www.fuzzwork.co.uk/dump/postgres-latest.dmp.bz2
+bzip2 -d postgres-latest.dmp.bz2
+pg_restore -i -h localhost -p 5432 -U spotmarketadmin -d spotmarket -v "postgres-latest.dmp"
+rm postgres-latest.dmp
