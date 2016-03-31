4f998e8512b965608b54d3faac6bf9fd1972b8ee
Brent Nowak <brent613@gmail.com>
2016-03-30 21:40:17 -0700

    killmailsum process error handling

--- process_killmailssum.py
+++ process_killmailssum.py
@@ -17,8 +17,8 @@ while minkmid <= maxkmid:
         timestamp = timestamp.replace('"', '').replace('.', '-')
         timestamp = arrow.get(timestamp)
         progress = str("{0:.3f}".format(float(minkmid) / float(maxkmid) * 100)) + "%"
-        print("[" + str(progress) + "][killID:" + str(kill['killID']) + "]")
+        print("[" + str(progress) + "][killID:" + str(kill['killID']) + "][insert]")
         insertkillmailsumrecord(kill['killID'], kill['characterid'], kill['corporationid'], kill['typeid'], kill['attackercount'], kill['damagetaken'], kill['timestamp'], kill['solarsystemid'], kill['x'], kill['y'], kill['z'])
     else:
-        print("[km][empty JSON]")
+        print("[" + str(progress) + "][killID:" + str(kill['killID']) + "][empty JSON]")
     minkmid += 1
