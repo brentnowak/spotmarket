187445f659dfca3ff1a87e83c5c7528b0e043356
Brent Nowak <brent613@gmail.com>
2016-04-09 09:27:16 -0700

    create supervisor file

--- consumer_charorders.py
+++ consumer_charorders.py
@@ -8,6 +8,8 @@
 
 from _utility import *
 from _consumer_charorders import *
+from time import sleep
+import sys
 import evelink.char
 import evelink.api
 
@@ -56,3 +58,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+    # Sleep for 1 hour + extra before ending and triggering another run via supervisor
+    print("[Completed Run:Sleeping for 1 Hour]")
+    sys.stdout.flush()
+    sleep(3900)
